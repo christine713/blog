0e586f627477ada1d62cd3a0c37dc3897acb9b39
Developer Experience team at Tendermint <hello@tendermint.com>
2022-03-14 03:38:34 +0000

    Initialized with Starport

---
+++ x/blog/genesis_test.go
@@ -0,0 +1,29 @@
+package blog_test
+
+import (
+	"testing"
+
+	keepertest "github.com/cosmonaut/blog/testutil/keeper"
+	"github.com/cosmonaut/blog/testutil/nullify"
+	"github.com/cosmonaut/blog/x/blog"
+	"github.com/cosmonaut/blog/x/blog/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGenesis(t *testing.T) {
+	genesisState := types.GenesisState{
+		Params: types.DefaultParams(),
+
+		// this line is used by starport scaffolding # genesis/test/state
+	}
+
+	k, ctx := keepertest.BlogKeeper(t)
+	blog.InitGenesis(ctx, *k, genesisState)
+	got := blog.ExportGenesis(ctx, *k)
+	require.NotNil(t, got)
+
+	nullify.Fill(&genesisState)
+	nullify.Fill(got)
+
+	// this line is used by starport scaffolding # genesis/test/assert
+}
